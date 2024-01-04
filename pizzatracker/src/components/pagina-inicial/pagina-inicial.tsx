8baa1c55651e040256e620a8c0437bedf7ef42d7
iza <115378274+izabartolini@users.noreply.github.com>
2024-01-04 16:53:05 -0200

    Criação da pagina inicial e configuracao scss

---
+++ pizzatracker/src/components/pagina-inicial/pagina-inicial.tsx
@@ -0,0 +1,12 @@
+import './pagina-inicial.module.scss';
+
+export default function TelaInicial() {
+
+  return (
+    <div className="style.container">
+      <nav>
+        PizzaTracker
+      </nav>
+    </div>
+
+ ); }
---
+++ pizzatracker/src/pages/telaInicial/tela-incial.tsx
@@ -0,0 +1,11 @@
+import TelaInicial from "@/components/pagina-inicial/pagina-inicial";
+
+
+export default function PaginaInicial() {
+  return (
+    <main>
+      
+      <TelaInicial></TelaInicial>
+    </main>
+  )
+}
