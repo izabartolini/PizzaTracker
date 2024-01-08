df358de095aa5ea9fcc2a4340472c6a15e5e4b45
Pontim56 <leonardopontimp@hotmail.com>
2024-01-08 13:05:58 -0300

    criação da tela inicial
    Dependencias: Botão adicionar aparecer quando aparter adicionar, fazer esse valor adicionado seu passado ao componente pessoa.
    passar a quatnidade de pessoas que participarão do rodizio para a pagina principal, e fazer ela criar essse valor em cards pessoas

--- pizzatracker/src/components/principal/Adicionar.tsx
+++ pizzatracker/src/components/principal/Adicionar.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 export default function Adicionar(){
+
+    const [valor, setValor] = useState("");
+
+    const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) =>{
+      setValor(event.target.value)
+    }
+
     function Adiciona(){
         <div className={`
             bg-amarelo mx-6 h-24 rounded-l-full rounded-r-3xl
             flex justify-start items-center text-black font-bold gap-3
             ` }>
                 <button className="bg-vermelho text-white ml-4 text-5xl w-16 h-16 rounded-full"><h1 className="text-center h-full">X</h1></button>
+
                 <button className="bg-verde text-white text-5xl w-16 h-16 rounded-full"><h1 className="text-center h-full">/</h1></button>
-                <h1 className="text-2xl font-bold bg-amareloclaro rounded-md w-36 border-2  border-black mt-1 truncate text-center">
-                    Nome
-                </h1>
+
+                <input className="text-2xl font-bold bg-amareloclaro rounded-md w-36 border-2  border-black mt-1 truncate text-center"
+                value={valor}
+                type="text" 
+                onChange={handleChange}
+                />
             </div>
     }
     return(
--- pizzatracker/src/pages/paginaPrincipal/principal.tsx
+++ pizzatracker/src/pages/paginaPrincipal/principal.tsx
@@ -1,17 +1,23 @@
-import Mostrar from "@/components/inicial/Mostrar";
+import Mostrar from "@/components/principal/Mostrar";
 import Nav from "@/components/geral/Nav";
-import Pessoa from "@/components/inicial/Pessoa";
-import Adicionar from "@/components/inicial/Adicionar";
+import Pessoa from "@/components/principal/Pessoa";
+import Adicionar from "@/components/principal/Adicionar";
 
-export default function PaginaInicial(){
+export default function PaginaPrinciapal(){
+    interface PrincipalProps{
+        nome: string;
+    }
+    
     return(
         <div className="flex flex-col justify-between min-h-screen gap-5 bg-branco overflow-y-auto">
             <Nav></Nav>
-            <Pessoa nome="HA"></Pessoa>
+            <Pessoa nome="{}"></Pessoa>
             <Pessoa nome="HAHAHA"></Pessoa>
             <Pessoa nome="AHAHAHAHAH"></Pessoa>
             <Pessoa nome="fauwge"></Pessoa>
             <Pessoa nome="fauwge"></Pessoa>
+            
+            
             <Adicionar></Adicionar>
             <Mostrar></Mostrar>
         </div>
